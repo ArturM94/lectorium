52a1d2895d7cddd79fd5b48b20ab06d9f01696da
Artur Manukian <manukian.artur.94@gmail.com>
2020-01-26 22:41:39 +0200

    feature: add db connection config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,15 @@
+import { ConnectionOptions } from 'typeorm';
+
 export const appConfig = {
   HOST: process.env.HOST || 'localhost',
   PORT: process.env.PORT || '3000',
+};
+
+export const dbConnectionConfig: ConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'lectorium',
 };
