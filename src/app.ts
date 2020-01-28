ba34ee503fbcb03782c70bf563faa8563ffa1bfc
Artur Manukian <manukian.artur.94@gmail.com>
2020-01-29 00:48:10 +0200

    feature: add invoke db connection function in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import { appConfig } from '@config/app';
+import { initiateDbConnection } from '@db/connection';
 
 const { PORT } = appConfig;
 const app = express();
+initiateDbConnection();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
