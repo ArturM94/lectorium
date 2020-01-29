4e88e4c48fee75702ac217b253bab4ab5fac3133
Artur Manukian <manukian.artur.94@gmail.com>
2020-01-29 23:26:45 +0200

    feature: create LessonRouter

---
+++ src/routes/lesson.ts
@@ -0,0 +1,16 @@
+import express, { Router } from 'express';
+
+import { LessonController } from '@controllers/lesson';
+
+export const LessonRouter: Router = express.Router();
+const lessonController: LessonController = new LessonController();
+
+LessonRouter.get('/', lessonController.getAll);
+
+LessonRouter.get('/:id', lessonController.getById);
+
+LessonRouter.post('/', lessonController.post);
+
+LessonRouter.put('/:id', lessonController.putById);
+
+LessonRouter.delete('/:id', lessonController.deleteById);
