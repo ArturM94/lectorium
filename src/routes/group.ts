db3f417772b23faca9f7e455c3769ed0692b9650
Artur Manukian <manukian.artur.94@gmail.com>
2020-01-29 23:26:24 +0200

    feature: create GroupRouter

---
+++ src/routes/group.ts
@@ -0,0 +1,16 @@
+import express, { Router } from 'express';
+
+import { GroupController } from '@controllers/group';
+
+export const GroupRouter: Router = express.Router();
+const groupController: GroupController = new GroupController();
+
+GroupRouter.get('/', groupController.getAll);
+
+GroupRouter.get('/:id', groupController.getById);
+
+GroupRouter.post('/', groupController.post);
+
+GroupRouter.put('/:id', groupController.putById);
+
+GroupRouter.delete('/:id', groupController.deleteById);
