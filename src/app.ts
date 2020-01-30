ab363087a3ee2ae70f0e05b431b7eabc31e2b8f8
Artur Manukian <manukian.artur.94@gmail.com>
2020-01-30 23:13:40 +0200

    feature: connect middlewares in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,29 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { ErrorRequestHandler, RequestHandler } from 'express';
 
 import { ApiRouter } from '@routes/api';
 import { appConfig } from '@config/app';
 import { initiateDbConnection } from '@db/connection';
+import JWT from '@middlewares/auth/JWT';
+import { AttachCurrentUser } from '@middlewares/auth/AttachCurrentUser';
+import { Forbidden, InternalServer, Unauthorized } from '@middlewares/ErrorHandlers';
 
 const { PORT } = appConfig;
 const app = express();
+
+const authMiddlewares: RequestHandler[] = [JWT, AttachCurrentUser];
+const errorMiddlewares: ErrorRequestHandler[] = [
+  Unauthorized,
+  Forbidden,
+  InternalServer
+];
+
 initiateDbConnection();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', ApiRouter);
+app.use('/api', authMiddlewares, ApiRouter);
+app.use(errorMiddlewares);
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
